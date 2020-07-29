ec27208aeb73d5ca4bd141e878a3f0c467d280c3
Nick Lanese <nlanese21@gmail.com>
2020-07-29 21:14:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -78,7 +78,7 @@ class Song
         end
       end
       if !(hasAlready)
-        @@genre_count[gen] => 1
+        @@genre_count[gen] = 1
       else
       #  thisGenreString = gen.to_s
       #  @@genre_count.each do | genreInCount , value |
@@ -89,6 +89,7 @@ class Song
       #  end
       end
     end
+    return @@genre_count
   end
 
   def name=(input)
